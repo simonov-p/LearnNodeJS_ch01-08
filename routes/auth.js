5428c0bfd3ffbf38fe8045c88b477253d2f335db
Petr <simonov-p@ya.ru>
2020-04-20 18:28:37 +0300

    56. Регистрация пользователя

--- routes/auth.js
+++ routes/auth.js
@@ -20,10 +20,30 @@ router.post('/login', async (req, res) => {
     });
 });
 
+
 router.get('/logout', async (req, res) => {
     req.session.destroy(() => {
         res.redirect('/auth/login#login')
     })
+});
+
+router.post('/register', async (req, res) => {
+    try {
+        const {email, password, repeat, name} = req.body;
+        const candidate = await User.findOne({email});
+
+        if (candidate) {
+            res.redirect('/auth/login#register')
+        } else {
+            const user = new User({
+                email, name, password, cart: {items: []}
+            });
+            await user.save();
+            res.redirect('/auth/login#login')
+        }
+    } catch (e) {
+        console.log(e)
+    }
 });
 
 module.exports = router;
