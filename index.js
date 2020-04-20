dd09310a39e8776fab9fe102b15be453e889f6c3
Petr <simonov-p@ya.ru>
2020-04-20 21:05:09 +0300

    59. Добавление CSRF защиты

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const csrf = require('csurf');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
@@ -39,6 +40,7 @@ app.use(session({
     saveUninitialized: false,
     store
 })); // можем обращаться к Request session
+app.use(csrf())
 app.use(varMiddleware);
 app.use(userMiddleeare);
 
