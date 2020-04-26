37e62949ac951aae1cc339e4b3e76b79c0449e31
Petr <simonov-p@ya.ru>
2020-04-26 13:41:05 +0300

    63. Настойка почтового сервиса. Sendgrid

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB_URI: `mongodb+srv://user:QzcUQhcbz736ZL7h@cluster0-mrk1p.mongodb.net/shop`,
-    SESSION_SECRET: 'some secret parameter'
+    SESSION_SECRET: 'some secret parameter',
+    SENDGRIP_API_KEY: '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'
 };
